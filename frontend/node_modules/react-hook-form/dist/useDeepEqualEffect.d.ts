320c2df0ea93a2d98c553652e19f9f16dcea49df
wgg76 <wgerhardt@gmail.com>
2025-05-02 22:33:14 -0300

    chore: add frontend lockfile & configure root postinstall

--- frontend/node_modules/react-hook-form/dist/useDeepEqualEffect.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-export declare const useDeepEqualEffect: <T extends React.DependencyList>(effect: React.EffectCallback, deps: T) => void;
-//# sourceMappingURL=useDeepEqualEffect.d.ts.map
